f8c30872cc1e7f068caf8337f00275f9bd2e3b1e
MinjaeKim <55883282+m339kim@users.noreply.github.com>
2021-10-11 14:52:15 -0400

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -5,17 +5,18 @@ from selenium.common.exceptions import TimeoutException
 from selenium/webdriver.common.desired_capabilities import DesiredCapabilities 
 
 class craigslist_crawler(object):
+    # if called, query and max_price goes into the corresponding position of the url.
     def __init__(self, query, max_price):
         self.max_price = max_price
         self.query = query
         self.url = f"https://kitchener.craigslist.org/search/sss?query={query}&max_price={max_price}"
-        self.driver = webdriver.Chrome(C:/Driverchromedriver.exe)
-        self.delay = 5
+        self.driver = webdriver.Chrome(C:/Driverchromedriver.exe) # Chome WebDriver, name and location in computer
 
     def load_page(self):
         driver = self.driver
-        driver.get(self.url)
+        driver.get(self.url) # access via url
         all_data = driver.find_elements_by_class_name("result-row")
+        # applied to each result-row
         for data in all_data:
             title = data.text.split("$")
             if title[0] == "":
